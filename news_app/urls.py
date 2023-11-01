174727c6500bee51b447020a6187fea72bad7d8b
Akhambay <assyl.akhambay@gmail.com>
2023-11-02 00:51:10 +0600

    Add Delete post function

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, NewsDetailView, AddNewsView, UpdateNewsView
+from .views import HomeView, NewsDetailView, AddNewsView, UpdateNewsView, DeleteNewsView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('news/<int:pk>', NewsDetailView.as_view(), name='news-details'),
     path('add_news/', AddNewsView.as_view(), name='add_news'),
     path('news/edit/<int:pk>', UpdateNewsView.as_view(), name='update_news'),
+    path('news/<int:pk>/delete/', DeleteNewsView.as_view(), name='delete_news'),
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 
 class NewsDetailView(DetailView):
@@ -26,3 +28,9 @@ class UpdateNewsView(UpdateView):
     form_class = PostForm
     template_name = 'update_news.html'
     # fields = ['title', 'title_tag', 'body']
+
+
+class DeleteNewsView(DeleteView):
+    model = Post
+    template_name = 'delete_news.html'
+    success_url = reverse_lazy('home')
