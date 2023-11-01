74ddc626d4428a7ce4ed09e6a3ffe0fe9f16448d
Akhambay <assyl.akhambay@gmail.com>
2023-11-01 16:39:47 +0600

    News detailed view added, Link to main page added

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import HomeView, NewsDetailView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
+    path('news/<int:pk>', NewsDetailView.as_view(), name='news-details'),
 ]
