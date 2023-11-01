d1dea6cea5c9cf52523b06b6aa921e315c515c14
Akhambay <assyl.akhambay@gmail.com>
2023-11-01 23:27:48 +0600

    Add form to post news

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, NewsDetailView
+from .views import HomeView, NewsDetailView, AddNewsView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('news/<int:pk>', NewsDetailView.as_view(), name='news-details'),
+    path('add_news/', AddNewsView.as_view(), name='add_news')
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -11,3 +11,9 @@ class HomeView(ListView):
 class NewsDetailView(DetailView):
     model = Post
     template_name = 'news_details.html'
+
+
+class AddNewsView(CreateView):
+    model = Post
+    template_name = 'add_news.html'
+    fields = '__all__'
