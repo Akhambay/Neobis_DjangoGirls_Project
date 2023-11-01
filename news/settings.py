5e520aadd38729ee7f3d2650623fa217aeecc98d
Akhambay <assyl.akhambay@gmail.com>
2023-11-02 01:21:20 +0600

    Add Sign Up & Log In functions

--- news/settings.py
+++ news/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news_app',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'Asia/Almaty'
 
