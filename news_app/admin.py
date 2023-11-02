3276ea3f37fe2b05d3521810306da98021ae3cd3
Akhambay <assyl.akhambay@gmail.com>
2023-11-02 18:53:58 +0600

    Update homepage view

--- news_app/admin.py
+++ news_app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
-
-admin.site.register(Post)
 # Register your models here.
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'title_tag', 'post_date')
+    search_fields = ['title', 'body']
+
+
+admin.site.register(Post, NewsAdmin)
